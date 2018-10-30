54d88425a77c74d8430d60b899bf7c08aa5056a3
anhcuongsuper <cuongnld00635@fpt.edu.vn>
2018-10-30 16:22:08 +0700

    ok

---
+++ js.js
@@ -0,0 +1,86 @@
+var musiclist = new Array (
+{
+    id:1540304588828,
+    Name : "Tình Ca",
+    Description:"Tình Ca",
+    Singer : "Trọng Hiếu",
+    Author : "Quốc Bảo",
+    Thumbnail: "http://static.amusic.vn/amusic/albums/s2/10/82660/82660.jpg?v=1523961932",
+    Link:"https://s1.vocaroo.com/media/download_temp/Vocaroo_s1cqKcLqsxJ5.mp3",
+    MemberId:1538655637969,
+    CreatedAt:"2018-10-23T14:23:08.828+0000",
+    UpdatedAt:"2018-10-23T14:23:08.828+0000",
+    Sstatus:'1'
+
+},
+{
+    id:1540304588828,
+    Name:"TÌnh Ca ",
+    Description:"Tình Ca",
+    Singer:"Trọng Hiếu",
+    Author:"Quốc Bảo",
+    Thumbnail:"http://static.amusic.vn/amusic/albums/s2/10/82660/82660.jpg?v=1523961932",
+    Link:"https://s1.vocaroo.com/media/download_temp/Vocaroo_s1cqKcLqsxJ5.mp3",
+    MemberId:1538655637969,
+    CreatedAt:"2018-10-23T14:23:08.828+0000",
+    UpdatedAt:"2018-10-23T14:23:08.828+0000",
+    Status:1
+},
+    {
+        id: 1540304628575,
+        name: "Nếu ",
+        description: "Hay",
+        singer: "Marzuz",
+        author: "Marzuz",
+        thumbnail: "http://sohanews.sohacdn.com/thumb_w/660/2018/3/2/photo1519952979461-15199529794612077336691.jpg",
+        link: "http://data2.chiasenhac.com/downloads/1725/2/1724970-05a36978/128/Neu%20-%20Onionn_%20Marzuz.mp3",
+        memberId: 1539698396527,
+        createdAt: "2018-10-23T14:23:48.575+0000",
+        updatedAt: "2018-10-23T14:23:48.575+0000",
+        status: 1
+    },
+    {
+        id:1540304709939,
+        name:"Sài Gòn Tôi Mưa ",
+        description:"Nằm trong bốn bức tường hai tư giờ\rChợt ngoài hiên xáo động tiếng mưa bất ngờ\rChiều nay giông đến sớm hơn mọi khi\rCầm ô tôi lê bước đi.\r\rĐường chật xe nháo nhào với cơn mưa rào\rNgười loay hoay cuống cuồng trú thân chốn nào\rÀo ào mưa rớt xuống đây\rSài Gòn lại thế.\r\r\rPre chorus:\r\rHm….\rCó ai nghe tiếng tôi tỉ tê \rHm….\rLúc cơn mưa chắn ai đường về\rHm….\rCó ai âm thầm để vài giọt lệ rơi\rGiữa chốn đông người, cảm thấy xa lạ, cảm thấy như vỡ oà.\r\rChorus:\r\rVì mình đã quá…cô đơn rồi\rGiữa phố hoa xốn xang nơi này\rTrong suốt những tháng ngày mỏng manh mỏng manh\rĐành phải tự thương vỗ về trấn an thân này.\rTự lau nước mưa và nước mắt từng chiều rơi xuống\rLàm sao đây, Sài Gòn mưa rơi, là mình lại yếu đuối.\r\r\rPre chorus:\r\rHm….\rCó ai nghe tiếng tôi tỉ tê \rHm….\rLúc cơn mưa chắn ai đường về\rHm….\rCó ai âm thầm để vài giọt lệ rơi\rGiữa chốn đông người, cảm thấy xa lạ, cảm thấy như vỡ oà.\r\rChorus:\r\rVì mình đã quá…cô đơn rồi\rGiữa phố hoa xốn xang nơi này\rTrong suốt những tháng ngày mỏng manh mỏng manh\rĐành phải tự thương vỗ về trấn an thân này.\rTự lau nước mưa và nước mắt từng chiều rơi xuống\rLàm sao đây, Sài Gòn mưa rơi, là mình lại yếu đuối.\r\rBridge:\rMuốn tan như mưa chẳng hoài buồn suy nghĩ\rMấy năm trôi qua tôi đã làm được gì\rPhố xa người lạ tôi muốn về nhà\rSài Gòn mưa, mưa, cho lòng người thêm ướt.\r\rChorus:\rVì mình đã hoài trôi giữa một khoảng không rất lạ\rVài ba người chắp tay mặt mừng\rNhưng đến khi xót xa biết ai mới là bạn.\r\rĐành phải tự thương vỗ về trấn an thân này.\rTự lau nước mưa và nước mắt từng chiều rơi xuống\rLàm sao đây, Sài Gòn mưa rơi, là mình lại yếu đuối.",
+        singer:" Nguyễn Kim Tuyên",
+        author:" Nguyễn Kim Tuyên",
+        thumbnail:"https://i1.sndcdn.com/artworks-000337189770-otd7tc-t500x500.jpg",
+        link:"https://od.lk/s/MTJfNjY3NTgxMV8/S%C3%A0i%20G%C3%B2n%20T%C3%B4i%20M%C6%B0a%20-%20Nguy%E1%BB%85n%20Kim%20Tuy%C3%AAn.mp3",
+        memberId:1538655637969,
+        createdAt:"2018-10-23T14:25:09.939+0000",
+        updatedAt:"2018-10-23T14:25:09.939+0000",
+        status:1
+    },
+    {
+        id:1540307622995,
+        name:"Kiss and Make up",
+        description:"Kiss and Make up",
+        singer:"Dua Lipa & BLACKPINK",
+        author:"Dua Lipa & BLACKPINK",
+        thumbnail:"https://pbs.twimg.com/media/DoiQ8WAXUAc2-lW.jpg",
+        link:"https://musicboss.org/get/music/20181020/musicbossorg_Dua_Lipa_Blackpink_-_Kiss_and_Make_Up_59804779.mp3",
+        memberId:1539103112760,
+        createdAt:"2018-10-23T15:13:42.995+0000",
+        updatedAt:"2018-10-23T15:13:42.995+0000",
+        status:1
+    }
+);
+var  music = document.getElementById('music');
+for (var i = 0; i< musiclist.length;i++){
+ var mp3 =  '<p>'+'id :' + musiclist[i].id +' '+
+            '<p>'+'Name:' +musiclist[i].Name+'</p>' +
+             '<p> '+'Description:'  + musiclist[i].Description+'</p>'+
+             '<p> ' + 'Singer:' + musiclist[i].Singer+'  </p>' +
+             '<p> '+' Author:' + musiclist[i].Author + ' </p>' +
+             '<img src="'+musiclist[i].Thumbnail +' + ">' +
+             '<audio controls autoplay="autoplay">'+
+            ' <source src="'+musiclist[i].link+'" type="audio/mpeg">\n' +
+            ' Your browser does not support the audio element.'+
+            ' </audio>'+
+            '<p> '+'Description:'  + musiclist[i].Description+'</p>'
+
+
+             ;
+music.innerHTML += mp3
+}
